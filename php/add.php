8760eaefd4e793dea0295a230fa07525677e280e
陆秋石 <uicestone@gmail.com>
2013-11-25 16:13:07 +0800

    合并php配置文件

--- php/add.php
+++ php/add.php
@@ -1,14 +1,10 @@
 <?php
+  require_once './config.php';
+  
   $name=$_POST["name"];
   $phno=$_POST["phno"];
   $email=$_POST["email"];
   $address=$_POST["address"];
-  $con=mysql_connect("127.0.0.1","root","0310");
-  if(!$con){
-    die('could not connect'. mysql_error());
-  }
-  mysql_query("set names 'utf8'");
-  mysql_select_db("test",$con);
   $search="select * from client_contact where (name='$name' and phno='$phno' and email='$email' and address='$address')";
   $result=mysql_query($search,$con);
   $row=mysql_fetch_array($result);
@@ -17,5 +13,6 @@
     $add_sql="insert into client_contact values ('$name','$phno','$email','$address')";
     mysql_query($add_sql,$con);
   }
-  mysql_close($con);
+  
+  require_once './destruct.php';
 ?>
--- php/edit.php
+++ php/edit.php
@@ -1,16 +1,12 @@
 <?php
+  require_once './config.php';
+  
   $name=$_POST["name"];
   $phno=(int)$_POST["phno"];
   $email=$_POST["email"];
   $address=$_POST["address"];
-  $con=mysql_connect("127.0.0.1","root","0310");
-  if(!$con){
-    die('could not connect'. mysql_error());
-  }
-  ;
-  mysql_query("set names 'utf8'");
-  mysql_select_db("test",$con);
   $update="update client_contact set phno='$phno', email='$email', address='$address' where name='$name'";
   mysql_query($update,$con);
-  mysql_close($con);
+  
+  require_once './destruct.php';
 ?>
